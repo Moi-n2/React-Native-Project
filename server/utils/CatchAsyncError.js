b36c36e0bee41b37bfb1fd3e37e1d1e59ce03515
Moi-n2 <taoqin9393@gmail.com>
2024-10-17 15:16:09 +0800

    update path

--- server/utils/CatchAsyncError.js
+++ server/utils/CatchAsyncError.js
@@ -1,5 +1,5 @@
-const CatchAsyncError = (fn) => (req, res, next) => {
+const catchAsyncError = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 };
 
-export default CatchAsyncError;
+export default catchAsyncError;
